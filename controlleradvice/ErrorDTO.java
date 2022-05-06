15a17b8d647cb9b7d9f066e7fa02ac35cb6e8b07
jaykab11 <86224237+jaykab11@users.noreply.github.com>
2022-05-06 09:34:43 +0200

    Add files via upload

---
+++ controlleradvice/ErrorDTO.java
@@ -0,0 +1,14 @@
+package com.evorra.backend.model.dao.companyUser;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorDTO
+{
+
+    public String status;
+    public String message;
+    public String time;
+}
